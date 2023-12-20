e76fc7091d8af11d2f422284a7caa64485a47c67
FranSecOps <franciscomancillajustme@gmail.com>
2023-12-20 09:12:39 -0500

    igw and rtb

--- main.tf
+++ main.tf
@@ -23,15 +23,22 @@ resource "aws_subnet" "myapp-subnet" {
     }
 }
 
-resource "aws_route_table" "myapp-rtb" {
+resource "aws_internet_gateway" "myapp-igw" {
     vpc_id = aws_vpc.myapp-vpc.id
-
-    route = {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.myapp-igw.id
+    tags = {
+      Name = "${var.env_prefix}-igw"
     }
 }
 
-resource "aws_internet_gateway" "myapp-igw" {
+resource "aws_route_table" "myapp-rtb" {
     vpc_id = aws_vpc.myapp-vpc.id
+
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+    tags = {
+      Name = "${var.env_prefix}-rtb"
+    }
 }
