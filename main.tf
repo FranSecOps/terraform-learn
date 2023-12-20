36db9ad7396f3379b606a3a4fc95b0de6b4fa54c
FranSecOps <franciscomancillajustme@gmail.com>
2023-12-20 06:59:55 -0500

    Terraform repository

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "aws"{
+    region = "us-east-1"
+}
+
+variable "cidr_blocks" {
+  description = "cidr blocks for vpcand subnet"
+  type = list(string)
+}
+
+resource "aws_vpc" "development-vpc"{
+    cidr_block = var.cidr_blocks[0]
+    tags = {
+        Name = "development-vpc"
+    }
+}
+
+resource "aws_subnet" "dev-subnet-1"{
+    vpc_id = aws_vpc.development-vpc.id
+    cidr_block = var.cidr_blocks[1]
+    availability_zone = "us-east-1a"
+    tags = {
+        Name = "subnet-1-dev"
+    }
+}
