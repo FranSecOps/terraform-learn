167d862d61b59deb4e92e414a7b134d467377e29
FranSecOps <franciscomancillajustme@gmail.com>
2023-12-20 09:34:06 -0500

    Default rtb

--- main.tf
+++ main.tf
@@ -30,15 +30,14 @@ resource "aws_internet_gateway" "myapp-igw" {
     }
 }
 
-resource "aws_route_table" "myapp-rtb" {
-    vpc_id = aws_vpc.myapp-vpc.id
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.id
 
-
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.myapp-igw.id
-    }
-    tags = {
-      Name = "${var.env_prefix}-rtb"
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw
+  }
+  tags = {
+    Name = "${var.env_prefix}-main-rtb"
+  }
 }
