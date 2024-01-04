5f6b1bcc1e653702739c26632079d28d4576e86a
FranSecOps <franciscomancillajustme@gmail.com>
2024-01-03 21:31:02 -0500

    High availability

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "aws"{
 }
 
 variable "vpc_cidr_block" {}
-variable "subnet_cidr_block" {}
-variable "avail_zone" {}
+variable "subnet_cidr_blocks" {}
+variable "avail_zones" {}
 variable "env_prefix" {}
 variable "my_ip" {}
 variable "instance_type" {}
@@ -17,12 +17,30 @@ resource "aws_vpc" "myapp-vpc" {
     }
   }
 
-resource "aws_subnet" "myapp-subnet" {
+resource "aws_subnet" "myapp-subnet-1" {
     vpc_id = aws_vpc.myapp-vpc.id
-    cidr_block = var.subnet_cidr_block
-    availability_zone = var.avail_zone
+    cidr_block = var.subnet_cidr_blocks[0]
+    availability_zone = var.avail_zones[0]
     tags = {
         Name = "${var.env_prefix}-subnet-1"
+    }
+}
+
+resource "aws_subnet" "myapp-subnet-2" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_blocks[1]
+    availability_zone = var.avail_zones[1]
+    tags = {
+        Name = "${var.env_prefix}-subnet-2"
+    }
+}
+
+resource "aws_subnet" "myapp-subnet-3" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_blocks[2]
+    availability_zone = var.avail_zones[2]
+    tags = {
+        Name = "${var.env_prefix}-subnet-3"
     }
 }
 
@@ -93,19 +111,53 @@ resource "aws_key_pair" "ssh-key"{
     public_key = file(var.public_key_location)
 }
 
-resource "aws_instance" "myapp-server" {
+resource "aws_instance" "myapp-server-1" {
     ami = data.aws_ami.latest-amazon-linux.id
     instance_type = var.instance_type
 
-    subnet_id = aws_subnet.myapp-subnet.id
+    subnet_id = aws_subnet.myapp-subnet-1.id
     vpc_security_group_ids = [aws_default_security_group.default-sg.id]
-    availability_zone = var.avail_zone
+    availability_zone = var.avail_zones[0]
 
     associate_public_ip_address = true
     key_name = aws_key_pair.ssh-key.key_name
     user_data = file("entry-script.sh")
 
     tags = {
-      Name = "${var.env_prefix}-server"
+      Name = "${var.env_prefix}-server-1"
+    }
+}
+
+resource "aws_instance" "myapp-server-2" {
+    ami = data.aws_ami.latest-amazon-linux.id
+    instance_type = var.instance_type
+
+    subnet_id = aws_subnet.myapp-subnet-2.id
+    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+    availability_zone = var.avail_zones[1]
+
+    associate_public_ip_address = true
+    key_name = aws_key_pair.ssh-key.key_name
+    user_data = file("entry-script.sh")
+
+    tags = {
+      Name = "${var.env_prefix}-server-2"
+    }
+}
+
+resource "aws_instance" "myapp-server-3" {
+    ami = data.aws_ami.latest-amazon-linux.id
+    instance_type = var.instance_type
+
+    subnet_id = aws_subnet.myapp-subnet-3.id
+    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+    availability_zone = var.avail_zones[2]
+
+    associate_public_ip_address = true
+    key_name = aws_key_pair.ssh-key.key_name
+    user_data = file("entry-script.sh")
+
+    tags = {
+      Name = "${var.env_prefix}-server-3"
     }
 }
